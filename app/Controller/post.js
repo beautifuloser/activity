b66b1a3301a14cbb88bd68be187e4a2405074420
beautifuloser <zhaoyachao@iMac.local>
2016-05-19 16:54:02 +0800

    angularjs
    angularjs jquery

---
+++ app/Controller/post.js
@@ -0,0 +1,14 @@
+module.exports = {
+    get : function (req, res, next) {
+        req.models.post.find()
+    },
+    put : function (req, res, next) {
+
+    },
+    delete : function (req, res, next) {
+
+    },
+    post : function (req, res, next) {
+
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var post = require('../app/Controller/post');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/posts',post.get);
+router.delete('/posts/:id',post.delete);
 module.exports = router;
