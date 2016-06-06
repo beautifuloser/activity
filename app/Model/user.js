99938a7ab7e6830a077ccaeef38e62236f6b6617
beautifuloser <466952688@qq.com>
2016-06-06 18:14:10 +0800

    userModel
    userModel

---
+++ app/Model/user.js
@@ -0,0 +1,16 @@
+var Waterline = require('waterline');
+module.exports = Waterline.Collection.extend({
+    identity : 'user',
+    connection : 'mongo',
+    schema : true,
+    attributes : {
+        openid:'string',//用户唯一标识
+        nickname: 'string',
+        sex:'string',
+        province:'string',
+        city:'string',
+        country:'string',
+        headimgurl:'string',
+        unionid: 'string'//只有在用户将公众号绑定到微信开放平台帐号后才会出现该字段
+    }
+});
